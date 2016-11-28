846a1543283d3d9323b04bed6c5e88585361e741
Asymons <frostdragonz@live.com>
2016-11-28 12:45:54 -0500

    working links for location

--- popup.js
+++ popup.js
@@ -3,9 +3,47 @@
  */
 
 $(function () {
-    var arr = [], l = document.links;
+    var arr = [], l = document.links, locations = {};
     console.log(l);
+
     setTimeout(function () {
+
+        $("tbody tr").each(function() {
+            var x = strip($(this).find("td:nth-child(6)").html()).replace(/\s+/g, '');
+            locations[x] = "https://uwaterloo.ca/map/"+ lettersString(x) + "-" + firstNumber(x) +"?basemap=D&room=" + lettersString(x) + "+" + numbersString(x);
+
+            //console.log(strip($(this).find("td:nth-child(6)").html()).replace(/\s+/g, ''));
+            //console.log("https://uwaterloo.ca/map/"+ lettersString(x) + "-" + firstNumber(x) +"?basemap=D&room=" + lettersString(x) + "+" + numbersString(x));
+
+            // Within tr we find the last td child element and get content
+
+        });
+
+        $.each(locations, function(index,value){
+            console.log(index);
+            console.log(value);
+
+
+            $("tbody tr td:nth-child(6)").each(function() {
+               // console.log(index);
+                //console.log(strip($(this).find("p")[0].innerHTML).replace(/\s+/g, ''));
+                 //console.log(strip($(this).find("p")[0]).replace(/\s+/g, '') == index);
+
+                if(strip($(this).find("p")[0].innerHTML).replace(/\s+/g, '') === index && strip($(this).find("p")[0].innerHTML).replace(/\s+/g, '') != null){
+                    var x = $(this).find("p").attr("class", index);
+
+                    for(var i = 0; i < document.getElementsByClassName(index).length; i++){
+                        document.getElementsByClassName(index)[i].innerHTML = "<a href=" + value + ">" + index  + "</a>";
+                    }
+                }
+
+            });
+
+
+
+            //console.log("https://uwaterloo.ca/map/"+ lettersString(value) + "-" + firstNumber(value) +"?basemap=D&room=" + lettersString(value) + "+" + numbersString(value));
+        });
+
         for (var i = 0; i < l.length; i++) {
             if (l[i].href.includes('#') && !l[i].innerText.includes('Did')) {
                 l[i].setAttribute('href', "http://www.ratemyprofessors.com/search.jsp?queryBy=teacherName&schoolName=university+of+waterloo&queryoption=HEADER&query=" + l[i].innerText + "&facetSearch=true");
@@ -15,6 +53,8 @@ $(function () {
                 arr.push(l[i].innerText);
             }
         }
+
+     //   https://uwaterloo.ca/map/MC-1?basemap=D&room=MC+1020
         //console.log(arr);
         //getProfURL(arr[1]);
     }, 1000);
@@ -36,6 +76,25 @@ $(function () {
             url: profURL,
             professorName: name
         };
+    }
+
+    function lettersString(str){
+        return str.replace(/[0-9]+/g, "");
+    }
+
+    function numbersString(str){
+        return str.replace(/[a-zA-Z]+/g, "");
+    }
+
+    function firstNumber(str){
+       return str.match(/\d/);
+    }
+
+    function strip(html)
+    {
+        var tmp = document.createElement("DIV");
+        tmp.innerHTML = html;
+        return tmp.textContent || tmp.innerText || "";
     }
 
 
